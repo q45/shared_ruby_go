46c04d84243b213f9995cde48dc7ee131b820261
Quintin Smith <quintin@getweave.com>
2015-09-24 09:57:16 -0600

    initial commit for shared library

---
+++ libsum.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"C"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+import "fmt"
+
+type Command struct {
+	URIs []string `json:"uris"`
+}
+
+type Result map[string]*Response
+type Response struct {
+	Body   string `json:"body"`
+	Status int    `json:"status"`
+	Err    string `json:"error"`
+	Uri    string `json:"uri"`
+}
+
+//export add
+// func add(a, b int) int {
+// 	return a + b
+// }
+
+//export fibonacci
+// func fibonacci(n int) int {
+// 	a := 1
+// 	b := 2
+
+// 	if n == 0 {
+// 		return 0
+// 	}
+
+// 	var i int
+// 	for i = 3; i < n; i++ {
+// 		c := a + b
+// 		a = b
+// 		b = c
+// 		fmt.Println(a)
+// 		fmt.Println(b)
+// 	}
+// 	return b
+// }
+
+func makeRequest(uri string) *Response {
+	resp, err := http.Get(uri)
+	if err != nil {
+		return &Response{Status: resp.StatusCode, Err: err.Error()}
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{Status: resp.StatusCode, Err: err.Error()}
+	}
+	fmt.Println(string(body))
+
+	return &Response{Uri: uri, Body: string(body), Status: resp.StatusCode}
+}
+
+//export scatter_request
+func scatter_request(data *C.char) string {
+	cmd := Command{}
+	err := json.Unmarshal([]byte(C.GoString(data)), &cmd)
+	if err != nil {
+		return err.Error()
+	}
+
+	c := make(chan *Response)
+	for _, uri := range cmd.URIs {
+		go func(_uri string) {
+			c <- makeRequest(_uri)
+		}(uri)
+	}
+
+	result := Result{}
+	for i := 0; i < len(cmd.URIs); i++ {
+		resp := <-c
+		result[resp.Uri] = resp
+	}
+
+	close(c)
+
+	b, err := json.Marshal(result)
+	if err != nil {
+		return "{}"
+	}
+
+	return string(b)
+}
+
+func main() {}
---
+++ test.rb
@@ -0,0 +1,33 @@
+require 'ffi'
+require 'json'
+require 'scatter'
+
+module Scatter
+	def self.request(uris)
+		ScatterBinding.scatter_request(JSON.dump({uris: uris}))
+	end
+
+	module ScatterBinding
+		extend FFI::Library
+		ffi_lib File.expand_path("./libscatter.so", File.dirname(__FILE__))
+		attach_function :scatter_request, [:string], :string
+	end
+end
+
+
+
+# module Sum
+# 	extend FFI::Library
+# 	ffi_lib './libsum.so'
+# 	# ffi_lib '/Users/q/go/src/github.com/q45/libsum/libsum.so'
+# 	attach_function :add, [:int, :int], :int
+# 	attach_function :fibonacci, [:int], :int
+# end
+
+urls = %w{http://ruby-lang.org http://rubygems.org http://golang.org}
+
+# puts Sum.add(15, 27)
+
+# puts Sum.fibonacci(10)
+
+puts Scatter.request(urls)
